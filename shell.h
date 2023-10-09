d03c77dbe14fafe528304e71d05f8a3bafa000ec
nzejoe <jonathannzete@gmail.com>
2023-10-09 10:39:31 -0700

    function prototypes created

--- shell.h
+++ shell.h
@@ -10,4 +10,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+extern char **environ;
+int _strmerge(const char *str1, const char *str2);
+void get_signal(int signal);
+size_t get_strlen(const char *str);
+char *_strdup(const char *str);
+char *strconcat(char *str_to, char *str_from);
+char *_strcpy(char *dest, char *src);
+ssize_t _getline(char **line, size_t *len, FILE *stream);
+
 #endif
