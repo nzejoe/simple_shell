3dd511632da411511727591c063af43ed6dcd316
nzejoe <jonathannzete@gmail.com>
2023-10-09 10:37:49 -0700

    imported required libraries for the project into the header file

--- shell.h
+++ shell.h
@@ -1,4 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 #endif
